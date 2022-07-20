b2339059ff9fa315fed4692b816f903158d09514
Muhammad Firmansyah <firman.rtm@gmail.com>
2022-07-20 08:21:13 +0700

    Initial commit

---
+++ app/src/main/java/com/bakudababel/bookingruangrapat/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bakudababel.bookingruangrapat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
